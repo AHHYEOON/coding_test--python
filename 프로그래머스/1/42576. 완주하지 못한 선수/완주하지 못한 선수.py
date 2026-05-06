153b5fcd79093cf6699b8d324cd5891dbdb0f6fc
AHHYEOON <130418823+AHHYEOON@users.noreply.github.com>
2026-05-06 13:25:26 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 75.08 ms, Memory: 25.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,11 +1,10 @@
 def solution(participant, completion):
-    count = {}
-    for name in participant:
-        count[name] = count.get(name,0) + 1
-    for name in completion:
-        count[name] -= 1
-    for key, value in count.items():
-        if value == 1:
-            return key
+    participant.sort()
+    completion.sort()
+    
+    for p, c in zip(participant, completion):
+        if p != c: 
+            return p
+    return participant[-1]
     
   
